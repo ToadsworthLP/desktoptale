0e626947f9c646ddb239cc28196e9ae7482255d5
ToadsworthLP <fiery.toadsworth@gmail.com>
2024-03-05 01:33:59 +0100

    Added Undertale to disclaimer

--- ProgramInfo.cs
+++ ProgramInfo.cs
@@ -5,5 +5,5 @@ public static class ProgramInfo
     public const string NAME = "Desktoptale";
     public const string AUTHOR = "ToadsworthLP";
     public const string VERSION = "1.0.0-dev";
-    public const string DESCRIPTION = "Characters and graphics created by Team UTY.\nNot affiliated with or endorsed by Toby Fox or Team UTY.";
+    public const string DESCRIPTION = "Characters and graphics created by Team UTY\nUNDERTALE created by Toby Fox\nNot affiliated with or endorsed by Team UTY or Toby Fox";
 }
