9fda134f1478ea9bdf6d1890af198a65123abaeb
ToadsworthLP <fiery.toadsworth@gmail.com>
2024-03-12 20:55:43 +0100

    Added Frisk/Toriel/Papyrus/Sans/Undyne

---
+++ Characters/Undyne.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Desktoptale.Characters
+{
+    public class Undyne : Character
+    {
+        public Undyne(CharacterCreationContext characterCreationContext) : base(characterCreationContext) {}
+    
+        public override void LoadContent(ContentManager contentManager)
+        {
+            OrientedAnimatedSprite idleSprite = new OrientedAnimatedSprite(
+                contentManager.Load<Texture2D>("Included/Undyne/Spr_Undyne_Walk_Up"), 4,    
+                contentManager.Load<Texture2D>("Included/Undyne/Spr_Undyne_Walk_Down"), 4,    
+                contentManager.Load<Texture2D>("Included/Undyne/Spr_Undyne_Walk_Left"), 2
+            );
+            idleSprite.Loop = false;
+            idleSprite.Framerate = 0;
+            IdleSprite = idleSprite;
+
+            OrientedAnimatedSprite walkSprite = new OrientedAnimatedSprite(
+                contentManager.Load<Texture2D>("Included/Undyne/Spr_Undyne_Walk_Up"), 4,    
+                contentManager.Load<Texture2D>("Included/Undyne/Spr_Undyne_Walk_Down"), 4,    
+                contentManager.Load<Texture2D>("Included/Undyne/Spr_Undyne_Walk_Left"), 2
+            );
+            walkSprite.Loop = true;
+            walkSprite.Framerate = 5;
+            walkSprite.StartFrame = 1;
+            WalkSprite = walkSprite;
+
+            OrientedAnimatedSprite runSprite = new OrientedAnimatedSprite(
+                contentManager.Load<Texture2D>("Included/Undyne/Spr_Undyne_Walk_Up"), 4,    
+                contentManager.Load<Texture2D>("Included/Undyne/Spr_Undyne_Walk_Down"), 4,    
+                contentManager.Load<Texture2D>("Included/Undyne/Spr_Undyne_Walk_Left"), 2
+            );
+            runSprite.Loop = true;
+            runSprite.Framerate = 10;
+            runSprite.StartFrame = 1;
+            RunSprite = runSprite;
+        }
+    }
+}
--- Registry/CharacterRegistry.cs
+++ Registry/CharacterRegistry.cs
@@ -4,6 +4,12 @@ namespace Desktoptale.Registry
 {
     public class CharacterRegistry : IntKeyRegistry<CharacterType>
     {
+        public static readonly CharacterType FRISK = new CharacterType("Frisk", "Undertale", ctx => new Frisk(ctx));
+        public static readonly CharacterType TORIEL = new CharacterType("Toriel", "Undertale", ctx => new Toriel(ctx));
+        public static readonly CharacterType PAPYRUS = new CharacterType("Papyrus", "Undertale", ctx => new Papyrus(ctx));
+        public static readonly CharacterType SANS = new CharacterType("Sans", "Undertale", ctx => new Sans(ctx));
+        public static readonly CharacterType UNDYNE = new CharacterType("Undyne", "Undertale", ctx => new Undyne(ctx));
+        
         public static readonly CharacterType CLOVER = new CharacterType("Clover", "Undertale Yellow", ctx => new Clover(ctx));
         public static readonly CharacterType DALV = new CharacterType("Dalv", "Undertale Yellow", ctx => new Dalv(ctx));
         public static readonly CharacterType MARTLET = new CharacterType("Martlet", "Undertale Yellow", ctx => new Martlet(ctx));
@@ -11,9 +17,15 @@ namespace Desktoptale.Registry
         public static readonly CharacterType CEROBA = new CharacterType("Ceroba", "Undertale Yellow", ctx => new Ceroba(ctx));
         public static readonly CharacterType AXIS = new CharacterType("Axis", "Undertale Yellow", ctx => new Axis(ctx));
         public static readonly CharacterType KANAKO = new CharacterType("Kanako", "Undertale Yellow", ctx => new Kanako(ctx));
-
+        
         public CharacterRegistry()
         {
+            Add(FRISK);
+            Add(TORIEL);
+            Add(PAPYRUS);
+            Add(SANS);
+            Add(UNDYNE);
+            
             Add(CLOVER);
             Add(DALV);
             Add(MARTLET);
