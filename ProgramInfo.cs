eaa230bb7d9e22b3a5d9d63900ae25d7728358f5
ToadsworthLP <fiery.toadsworth@gmail.com>
2024-03-14 17:08:46 +0100

    Added Chujin sprite credits

--- ProgramInfo.cs
+++ ProgramInfo.cs
@@ -4,13 +4,13 @@
     {
         public const string NAME = "Desktoptale";
         public const string AUTHOR = "ToadsworthLP";
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.1.0-beta1";
         public const string DISCLAIMER = "UNDERTALE and DELTARUNE created by Toby Fox\nUndertale Yellow created by Team UTY\nNot affiliated with or endorsed by Team UTY or Toby Fox";
         
-        public const string CREDITS = @"Credits:
-Team UTY - Undertale Yellow character graphics
-sdvlex, RedCactusSound, Comfnani - Beta testing
-yigarjpg - Starlo running animation
+        public const string CREDITS = @"Additional Credits:
+@sdvlex, @RedCactusSound, @Comfnani - Testing
+@yigarjpg - Custom Starlo running animation
+@Inky_125 - Custom Chujin sprites
 ";
     }
 }
